bb1ee00eeaa3abfb181d9910575b01287c8957b6
Lorenzo <lorenzozani1995@gmail.com>
2019-01-30 11:45:46 +0100

    FrontEnd CleanUp

--- todo-frontend/function.js
+++ todo-frontend/function.js
@@ -1,31 +1,11 @@
-function createToDoTextInput() {
-  const inputText = document.createElement("input");
-  inputText.type = "text";
-  inputText.size = 50;
-  return inputText;
-}
+//TODO ARRAY
+const todos = [];
 
-function createToDoAddBtn(toDoInputElement) {
-  const btn = document.createElement("input");
-  btn.type = "button";
-  btn.value = "ADD";
-  btn.style.margin = "10px";
-  btn.addEventListener("click", () => {
-    createToDo(toDoInputElement.value);
-  });
-  return btn;
-}
-
+//CREATING TODO&&TODOLIST
 function createToDo(text) {
   const newTodo = { checked: false, text };
   todos.push(newTodo);
   main();
-}
-
-function createToDoCheckBox() {
-  const checkBox = document.createElement("input");
-  checkBox.type = "checkBox";
-  return checkBox;
 }
 
 function createToDoAppDiv(todos) {
@@ -36,54 +16,82 @@ function createToDoAppDiv(todos) {
   div.appendChild(createToDoList(todos));
   return div;
 }
-const todos = [];
-
-function removeToDo(i) {
-  todos.splice(i, 1);
-}
 
 function createToDoList(todos) {
   const ul = document.createElement("ul");
-  for (const todo of todos) {
-    const label = document.createElement("label");
-    const labelText = document.createElement("span");
-    const check = createToDoCheckBox();
-    const li = document.createElement("li");
-    const deleteBtn = createToDoDeleteBtn();
-    let i = todos.indexOf(todo);
-    deleteBtn.style.display = "none";
-    label.appendChild(check);
-    labelText.innerText = todo.text;
-    labelText.appendChild(deleteBtn);
-    li.appendChild(label);
-    li.appendChild(labelText);
-    check.addEventListener("click", () => {
-      hideBtn(check, deleteBtn);
-    });
-    deleteBtn.addEventListener("click", () => {
-      ul.removeChild(li);
-      removeToDo(i);
-    });
-
-    ul.appendChild(li);
-  }
+  todos.forEach((todo, index) => {
+    ul.appendChild(createTodoElement(todo, index));
+  });
   return ul;
 }
-function hideBtn(check, deleteBtn) {
-  if (check.checked == true) {
-    deleteBtn.style.display = "inline";
-  } else {
-    deleteBtn.style.display = "none";
+
+//CREATING HTML ELEMENTS
+
+function createTodoElement(todo, index) {
+  //TODO BODY
+  const li = document.createElement("li");
+  li.appendChild(createToDoCheckBox(todo));
+  li.appendChild(document.createTextNode(todo.text));
+  if (todo.checked) {
+    li.appendChild(createToDoDeleteBtn(index));
   }
+  return li;
 }
 
-function createToDoDeleteBtn() {
+function createToDoTextInput() {
+  //TEXT
+  const inputText = document.createElement("input");
+  inputText.type = "text";
+  inputText.size = 50;
+  return inputText;
+}
+
+function createToDoAddBtn(toDoInputElement) {
+  //BUTTON TO ADD NEW TODO
+  const btn = document.createElement("input");
+  btn.type = "button";
+  btn.value = "ADD";
+  btn.style.margin = "10px";
+  btn.addEventListener("click", () => {
+    createToDo(toDoInputElement.value);
+  });
+  return btn;
+}
+
+function createToDoDeleteBtn(index) {
   const deleteBtn = document.createElement("input");
   deleteBtn.type = "button";
   deleteBtn.value = "DELETE";
+  deleteBtn.style.margin = "10px";
+  deleteBtn.style.backgroundColor = "red";
+  deleteBtn.addEventListener("click", () => {
+    deleteToDo(index);
+  });
   return deleteBtn;
 }
 
+function createToDoCheckBox(todo) {
+  const checkBox = document.createElement("input");
+  checkBox.type = "checkBox";
+  checkBox.style.margin = "10px";
+  checkBox.addEventListener("click", () => {
+    checkTodo(todo);
+  });
+  return checkBox;
+}
+
+//FUNCTIONS
+function checkTodo(todo) {
+  todo.checked = !todo.checked;
+  main();
+}
+
+function deleteToDo(index) {
+  todos.splice(index, 1);
+  main();
+}
+
+//PAGE CREATION
 function main() {
   document.body.innerHTML = "";
   document.body.appendChild(createToDoAppDiv(todos));
